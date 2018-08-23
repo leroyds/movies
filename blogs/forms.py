589111fc7abef2aaa23835b8d5884fa1cc1ffed3
Mcanthony <mcdemilroc@gmail.com>
2018-08-23 18:10:08 +0530

    added Comment form changes

--- blogs/forms.py
+++ blogs/forms.py
@@ -1,5 +1,7 @@
 from  django import forms
 from . import models
+from django.contrib.auth.models import User
+
 
 class create_post(forms.ModelForm):
 	class Meta:
@@ -7,6 +9,16 @@ class create_post(forms.ModelForm):
 		fields=['title','slug','description','release_date','language','tags','image']
 
 class create_comment(forms.ModelForm):
+	post_obj=forms.ModelChoiceField(
+		widget=forms.HiddenInput,
+		disabled=True,
+		queryset=models.Post.objects.all()
+	)
+	user_obj=forms.ModelChoiceFields(
+		widget=forms.HiddenInput,
+		disabled=True,
+		queryset=User.objects.all()
+	)
 	class Meta:
 		model = models.Comment
-		fields = ['comment_title','ratings','body']
+		fields = ['post_obj','user_obj','comment_title','ratings','body']
--- blogs/models.py
+++ blogs/models.py
@@ -43,7 +43,7 @@ class Comment(models.Model):
     post_obj        = models.ForeignKey(Post, related_name="post_comment", on_delete=models.CASCADE)
     user_obj        = models.ForeignKey(User,related_name='user_comment', on_delete=models.CASCADE)
     comment_title   = models.CharField(max_length=255)
-    ratings         = models.IntegerField(default=0)
+    ratings         = models.PositiveIntegerField(default=0)
     body            = models.TextField(default='')
     created_date    = models.DateTimeField( auto_now_add=True)
     updated_date    = models.DateTimeField(auto_now=True)
