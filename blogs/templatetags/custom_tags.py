2d7c23416747ab78bc6f25f2061785f920443055
Unknown <mcdemilroc@gmail.com>
2018-08-27 17:38:12 +0530

    added Summary panel on the right side of screen
    added custom tags

--- blogs/templatetags/custom_tags.py
+++ blogs/templatetags/custom_tags.py
@@ -4,6 +4,16 @@ from ..models import Post
 
 register=template.Library()
 
+@register.simple_tag(name='TotalPosts')
+def PostCount():
+    return Post.objects.count()
+
 @register.simple_tag(name='AVERAGE_RATINGS')
 def get_avg_ratings(pk):
     return Post.objects.get(pk=pk).post_comment.filter(comment_status=True).aggregate(avg_rating=Avg('ratings'))
+
+
+@register.inclusion_tag("blogs/latest_posts.html")
+def most_recent_added_movies(count=5):
+    latest_posts= Post.objects.all().order_by('-created_date')[:count]
+    return {'latest_posts':latest_posts}
